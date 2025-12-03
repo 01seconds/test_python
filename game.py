88a4a649b1cedd15ae5687d291093cb01e1bcdce
Anthony <dev@anthonydreier.com>
2025-12-03 18:42:03 -0500

    Fix error

--- game.py
+++ game.py
@@ -1,14 +1,9 @@
-<<<<<<< HEAD
-print("Welcome to Rock-Paper-Scissors!")
-
-p_input = input("Choose rock, paper, or scissors: ")
-
-print("You chose: " + p_input)
-=======
 import random
 
 print("Welcome to Rock-Paper-Scissors!")
 
 choice = random.choice(["rock", "paper", "scissors"])
+p_input = input("Choose rock, paper, or scissors: ")
+
 print("Computer chose: " + choice)
->>>>>>> computer
+print("You chose: " + p_input)
